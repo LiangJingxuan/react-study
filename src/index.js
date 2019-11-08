28f9100934253c43884dd9d0a91e3f95afc152a3
liangjiang <xuan03121994@163.com>
2019-11-08 15:35:04 +0800

    react

--- src/index.js
+++ src/index.js
@@ -1,12 +1,112 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {Component} from 'react'
+import {render} from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const app=<h1>react hello react11</h1>;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const creatApp = (props)=>{
+// 	return (
+// 		<div>
+// 			<h1>react hello {props.name}</h1>
+// 			<p>{props.name}</p>
+// 		</div>
+// 	)
+// }
+
+// const app=creatApp({
+// 	name: 'react hello'
+// })
+
+// 函数组件
+const App=(props)=>{
+	return (
+		<div>
+			<h1 title={props.name}>react hello {props.name}</h1>
+			<p>{props.name}</p>
+		</div>
+	)
+}
+
+// 对象组件
+class Context extends Component{
+	render(){
+		return (
+			<div>
+				<ul>
+					<li>{this.props.list}</li>
+				</ul>
+			</div>
+		)
+	}
+}
+class Body extends Component{
+	render(){
+		return (
+			<div className='body' title='jsx原理'>
+				<p>{this.props.desc}</p>
+				<span></span>
+				<Context list='子组件内容' />
+
+				<Jsxdom />
+			</div>
+		)
+	}
+}
+
+
+// jsx原理
+const Dom = {
+	tag: 'div',
+	attrs: {
+		className: 'body',
+		title: 'jsx原理'
+	},
+	children: [
+		{
+			tag: 'p',
+			attrs: {
+				title: 'jsx'
+			},
+			children: ['jsx原理']
+		},
+		{
+			tag: 'span',
+			attrs: null,
+			children: null
+		}
+	]
+}
+
+class Jsxdom extends Component {
+	render(){
+		return React.createElement(
+			'div',
+			{
+				className: 'body',
+				title: 'jsx原理'
+			},
+			React.createElement(
+				'p',
+				{
+					title: 'jsx原理'
+				},
+				'jsx原理内容...'	
+			),
+			React.createElement(
+				'span',
+				{
+					title: 'jsx原理'
+				},
+				'jsx原理内容...'
+			)
+		)
+	}
+}
+
+render(
+
+	<Body desc='react组件' />,
+
+	document.getElementById('root')
+
+);
+
